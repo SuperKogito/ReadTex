4cb00acbb895509984de6712ee0720460eddbfd8
Ayoub Malek <15731839+SuperKogito@users.noreply.github.com>
2018-05-13 22:33:29 +0200

    Improve code and delete unused code lines

--- logic.py
+++ logic.py
@@ -1,9 +1,8 @@
-from pydub import AudioSegment
 from gtts import gTTS
 
 
 class Reader():
-    
+    @classmethod
     def process(self, text):
         tts = gTTS(text=text, lang='en')
         tts.save("text_to_speech_file.mp3")
